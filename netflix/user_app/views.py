37c7edd3f23f40239ad30179bbff58f8b35c7a73
Evren <evrenogul20@gmail.com>
2024-03-11 16:20:34 +0300

    example

---
+++ netflix/user_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect
+from .forms import *
+# Create your views here.
+
+def register_page(request):
+    context = {}
+    context["registerForm"] = UserForm
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        #form geçerli mi değil mi
+        if form.is_valid():
+            #veritabanına kaydet
+            form.save()
+            #logine yönlendir
+            return redirect('page-index')
+        else:
+            #hata ver
+            print("FORM HATAASI",form.errors)
+            # return redirect('page-register')
+            context["errorForm"] = form.errors
+            return render(request,"register.html",context)
+
+    else:
+        #get ise sayfayı hazırla
+        # context["registerForm"] = UserForm
+        return render(request,"register.html",context)
+    
+
+def login_page(request):
+    return render(request,"login.html")
